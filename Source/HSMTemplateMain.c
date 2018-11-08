1cce9afa5e20dbea2b335b7d1795d1a35b54ec13
EVORA2\Ed <ecarryer@gmail.com>
2018-11-07 22:04:52 -0800

    updates the HSMTemplate Mais for changes mades to type etc

--- Source/HSMTemplateMain.c
+++ Source/HSMTemplateMain.c
@@ -14,16 +14,18 @@
  History
  When           Who     What/Why
  -------------- ---     --------
- 02/27/17 12:01 jec      set framework timer rate to 1mS
- 02/06/15 13:21 jec      minor tweaks to include header files & clock init for 
-                         Tiva
- 02/08/12 10:32 jec      major re-work for the Events and Services Framework
-                         Gen2
- 03/03/10 00:36 jec      now that StartTemplateSM takes an event as input
-                         you should pass it something.
- 03/17/09 10:20 jec      added cast to return from RunTemplateSM() to quiet
-                         warnings because now that function returns Event_t
- 02/11/05 16:56 jec      Began coding
+ 11/26/17 14:09 jec     updated with changes from main.c for headers & port 
+                        initialization
+ 02/27/17 12:01 jec     set framework timer rate to 1mS
+ 02/06/15 13:21 jec     minor tweaks to include header files & clock init for 
+                        Tiva
+ 02/08/12 10:32 jec     major re-work for the Events and Services Framework
+                        Gen2
+ 03/03/10 00:36 jec     now that StartTemplateSM takes an event as input
+                        you should pass it something.
+ 03/17/09 10:20 jec     added cast to return from RunTemplateSM() to quiet
+                        warnings because now that function returns Event_t
+ 02/11/05 16:56 jec     Began coding
 ****************************************************************************/
 #include <stdint.h>
 #include <stdbool.h>
